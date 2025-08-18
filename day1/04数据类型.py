4030cd0592f17acba5eab957421c0dfe223d5c96
GZY-q <2965753496@qq.com>
2025-08-18 18:52:57 +0800

    修改
    day1

---
+++ day1/04数据类型.py
@@ -0,0 +1,27 @@
+#int --整形
+num1 = 1
+num2 = 2.1
+print(type(num1))
+print(type(num2))
+# str 字符串
+a ="helloworld"
+print(type(a))
+# bool布尔型
+b = True
+print(type(b))
+
+# list 列表型
+c =[1,2,3]
+print(type(c))
+
+# tuple --元组
+d = {1,2,3}
+print(type(d))
+
+# set--- 集合
+e = [1,2,3]
+print(type(e))
+
+# dict ---字典 --- 键值对
+f = {"name":"tom", "age":18}
+print(type(f))
