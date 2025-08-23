39f35785a081e4f9e9e7810c02852a9a12823492
GZY-q <2965753496@qq.com>
2025-08-23 10:19:34 +0800

    修改

--- day6/04查找.py
+++ day6/04查找.py
@@ -1,14 +1,16 @@
-mystr = "hello world and itcast and itheima and Python"
+# mystr = "hello world and itcast and itheima and Python"
+#
+# # 1.find
+# print(mystr.find('and')) #12
+# print(mystr.find('and' , 15, 30)) #23
+# print(mystr.find('ands')) #-1 子串不存在
+#
+#
+#
+# # 2.index
+# print(mystr.index('and', 15, 30)) # 23
 
-# 1.find
-print(mystr.find('and')) #12
-print(mystr.find('and' , 15, 30)) #23
-print(mystr.find('ands')) #-1 子串不存在
 
-
-
-# 2.index
-print(mystr.index('and', 15, 30)) # 23
-
-
-
+mylist = ['aa', 'bb', 'cc']
+new_str = '...'.join(mylist)
+print(new_str)
